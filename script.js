152b74b86ea3188387c323006f79669d2eda4aa4
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2023-09-14 20:04:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+function backspace() {
+	let display = document.getElementById("display");
+	display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+	let display = document.getElementById("display");
+	let expression = display.value;
+	let result;
+
+	try {
+		// Convert trigonometric function inputs from degrees to radians
+		expression = expression.replace(/sin\(/g, 'sin(' + Math.PI / 180 + '*');
+		expression = expression.replace(/cos\(/g, 'cos(' + Math.PI / 180 + '*');
+		expression = expression.replace(/tan\(/g, 'tan(' + Math.PI / 180 + '*');
+
+		result = math.evaluate(expression);
+		display.value = result;
+	} catch (error) {
+		display.value = "Error";
+	}
+}
+
+function squareRoot() {
+	let display = document.getElementById("display");
+	display.value += "sqrt(";
+}
+
+function base10Log() {
+	let display = document.getElementById("display");
+	display.value += "log(";
+}
+
+function pi() {
+	let display = document.getElementById("display");
+	display.value += "pi";
+}
+
+function e() {
+	let display = document.getElementById("display");
+	display.value += "e";
+}
+
+function power() {
+	let display = document.getElementById("display");
+	display.value += "^(";
+}
